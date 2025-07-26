661b6ca8fa88a07ff3ace04329a9c9432bfce34e
 <raghak004@gmail.com>
2025-07-26 11:48:31 +0530

    week 6

---
+++ Cognizant/week 6/trainersapp/src/TrainersMock.js
@@ -0,0 +1,9 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+  new Trainer(1, "Kalikola Raghavendra Reddy", "raghavendra123@example.com", "1234567890", "React", ["JS", "Redux"]),
+  new Trainer(2, "Bhushan", "bhushan@example.com", "9876543210", "Angular", ["TS", "RxJS"]),
+  new Trainer(3, "John Wick", "wick@example.com", "5555555555", "Vue", ["Vuex", "Composition API"])
+];
+
+export default trainers;
